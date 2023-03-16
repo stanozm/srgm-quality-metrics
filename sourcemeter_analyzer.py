2bfb01ef0713f696d85c13639a56d5b1695e6dbb
stanozm <stanislavch@gmail.com>
2023-03-16 17:04:54 +0200

    Fixed SourceMeter execution for Java. Started adding support for Python and C#

--- sourcemeter_analyzer.py
+++ sourcemeter_analyzer.py
@@ -1,12 +1,17 @@
+import os
 import sys
 import subprocess as sp, shlex
 from github import Github
 
 import git
 
+JAVA_PROJECTS_LIST_FILE = 'projects-java-test.txt'
+PYTHON_PROJECTS_LIST_FILE = ''
+CSHARP_PROJRCTS_LIST_FILE = ''
 
-SOURCEMETER_PATH = "/utility/SourceMeter/SourceMeter-10.0.0-x64-linux/SourceMeter-10.0.0-x64-Linux/Java"
-SOURCEMETER_PARAMS = " -runFB=false" \
+
+SOURCEMETER_PATH = "/u/23/chrens1/unix/SourceMeter"
+SOURCEMETER_JAVA_PARAMS = " -runFB=false" \
                      " -runAndroidHunter=false" \
                      " -runVulnerabilityHunter=false" \
                      " -runFaultHunter=false" \
@@ -14,7 +19,15 @@ SOURCEMETER_PARAMS = " -runFB=false" \
                      " -runDCF=false" \
                      " -runMetricHunter=false" \
                      " -runMET=false"
-PROJECTS_DIR =  "/u/23/chrens1/unix/utility/SourceMeter/SourceMeter-10.0.0-x64-linux/SourceMeter-10.0.0-x64-Linux/Java/ESEM"
+
+SOURCEMETER_PYTHON_PARAMS = ''
+
+SOURCEMETER_CSHARP_PARAMS = ''
+
+
+PROJECTS_DIR =  "/u/23/chrens1/unix/SourceMeter/ESEM/projects"
+
+
 
 github = Github()
 
@@ -40,17 +53,54 @@ def get_repo_releases(github_repo):
     releases = github_repo.get_releases()
     return releases
 
-def execute_sourcemeter(project_name, project_dir, project_release):
-    command = f".{SOURCEMETER_PATH}/SourceMeterJava " \
-              + f"-projectName={project_name}-{project_release} " \
-              + f"-projectBaseDir={project_dir} " + f"-resultsDir={PROJECTS_DIR}/Results " + SOURCEMETER_PARAMS
+def execute_sourcemeter(project_name, project_dir, project_release,lang):
+    command = get_language_command(project_name, project_dir, project_release, lang)
     print('Executing command: ' + command)
-    sp.run(shlex.split(command), shell=True)
+    os.system(command)
 
 
+def get_language_command(project_name, project_dir, project_release, lang):
+    switch ={
+        'java': get_java_command(project_name,project_dir,project_release),
+        'python': get_python_command(project_name,project_dir,project_release),
+        'csharp': get_csharp_command(project_name,project_dir,project_release)
+    }
+    return switch.get(lang, 'Invalid language parameter')
 
-if __name__ == '__main__':
-    repo_names = parse_repo_names(sys.argv[1])
+def get_java_command(project_name, project_dir, project_release):
+     command = f"{SOURCEMETER_PATH}/Java/SourceMeterJava " \
+              + f"-resultsDir={PROJECTS_DIR}/../Results/Java " \
+              + f"-projectName={project_name} " \
+              + f"-currentDate={project_release} " \
+              + f"-projectBaseDir={project_dir} " + SOURCEMETER_JAVA_PARAMS
+     return command
+
+
+def get_python_command(project_name, project_dir, project_release):
+    command = f"{SOURCEMETER_PATH}/Python/SourceMeterPython " \
+              + f"-resultsDir={PROJECTS_DIR}/../Results/Python " \
+              + f"-projectName={project_name} " \
+              + f"-currentDate={project_release} " \
+              + f"-projectBaseDir={project_dir} " + SOURCEMETER_PYTHON_PARAMS
+    return command
+
+
+def get_csharp_command(project_name, project_dir, project_release):
+    command = f"{SOURCEMETER_PATH}/CSharp/SourceMeterCSharp " \
+              + f"-resultsDir={PROJECTS_DIR}/../Results/CSharp " \
+              + f"-projectName={project_name} " \
+              + f"-currentDate={project_release} " \
+              + f"-projectBaseDir={project_dir} " + SOURCEMETER_CSHARP_PARAMS
+    return command
+
+def analyze_projects(lang):
+    repo_names = []
+    if lang=='java':
+        repo_names = parse_repo_names(JAVA_PROJECTS_LIST_FILE)
+    if lang=='python':
+        repo_names = parse_repo_names(PYTHON_PROJECTS_LIST_FILE)
+    if lang=='csharp':
+        repo_names = parse_repo_names(CSHARP_PROJRCTS_LIST_FILE)
 
     for repo_name in repo_names:
         print('Processing repo: ' + repo_name)
@@ -62,7 +112,13 @@ if __name__ == '__main__':
             cloned_repo_tag_ref = cloned_repo.tags[rel.tag_name].commit
             cloned_repo.git.checkout(cloned_repo_tag_ref)
             repo_dir = cloned_repo.working_dir
-            execute_sourcemeter(repo_name.split("/")[1],repo_dir,rel.tag_name)
+            execute_sourcemeter(repo_name.split("/")[1],repo_dir,rel.tag_name, lang)
             print('-' * 20)
 
-    print(repo_names)
+
+
+if __name__ == '__main__':
+    analyze_projects('java')
+    #analyze_projects('python')
+    #analyze_projects('csharp')
+
